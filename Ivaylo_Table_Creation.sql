d00e3b866660aa50a892cf0c5b393b3dc6e503c2
Ivaylo-Valentinov <58509466+Ivaylo-Valentinov@users.noreply.github.com>
2020-05-05 11:52:20 +0300

    Update Ivaylo_Table_Creation.sql

--- Ivaylo_Table_Creation.sql
+++ Ivaylo_Table_Creation.sql
@@ -13,11 +13,12 @@ EGN varchar(10) NOT NULL,
 FirstName varchar(256) NOT NULL,
 LastName varchar(256) NOT NULL,
 Job varchar(32) NOT NULL,
-LengthOfService int,
+LengthOfService int, --days
 StartDate datetime NOT NULL,
 EndDate datetime);
 
 create table Payments(
+ID varchar(16) NOT NULL,
 Room int NOT NULL,
 GuestID varchar(16) NOT NULL,
 BaseFee float NOT NULL,
@@ -25,3 +26,15 @@ ExtraFee float,
 Method varchar(256),
 PaymentStatus bit,
 DueDate date NOT NULL);
+
+alter table Reservations add constraint PK_Reservations PRIMARY KEY (ID);
+alter table Reservations add constraint FK_Reservations_Guest FOREIGN KEY (GuestID) REFERENCES Guests(ID);
+alter table Reservations add constraint Check_Adults CHECK(Adults > 0);
+alter table Reservations add constraint Check_Children CHECK(Children >= 0);
+
+alter table Staff add constraint PK_Staff PRIMARY KEY (EGN);
+alter table Staff add constraint Check_LengthOfService CHECK(LengthOfService >= 0);
+
+alter table Payments add constraint PK_Payments PRIMARY KEY (ID);
+alter table Payments add constraint FK_Payments_Guest FOREIGN KEY (GuestID) REFERENCES Guests(ID);
+alter table Payments add constraint Check_PaymentMethod CHECK(Method in ('PayPal', 'VISA', 'MasterCard', 'Cash'));
