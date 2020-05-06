c87e0c86b902aeaa9796dd63e6d2b17a66fdd76d
Nikola <nikola.r.totev@gmail.com>
2020-05-06 14:43:02 +0300

    Update Nikola_Data_Insertion.sql

--- Nikola_Data_Insertion.sql
+++ Nikola_Data_Insertion.sql
@@ -11,13 +11,15 @@ INSERT INTO Rooms VALUES (2,'Double Room',0, 0);
 INSERT INTO Rooms VALUES (3,'Apartment',0, 0);
 INSERT INTO Rooms VALUES (4, 'Pent House',0, 0);
 INSERT INTO Rooms VALUES (5,'Single',0, 0);
-INSERT INTO Rooms VALUES (6,'Double Room',2, 0);
+INSERT INTO Rooms VALUES (6,'Double Room',0, 0);
 INSERT INTO Rooms VALUES (7,'Apartment',0, 0);
-INSERT INTO Rooms VALUES (8, 'Apartment',0, 0);
-INSERT INTO Rooms VALUES (9,'Apartment',0, 0,0);
-INSERT INTO Rooms VALUES (10, 'Single',0, 0,0);
-INSERT INTO Rooms VALUES (11,'Double Room',0, 0,0);
-INSERT INTO Rooms VALUES (12,'Apartment',0, 0,0);
-INSERT INTO Rooms VALUES (13, 'Single',0, 0,0);
-INSERT INTO Rooms VALUES (14,'Apartment',0, 0,0);
-INSERT INTO Rooms VALUES (15, 'Single',0, 0,0);
+INSERT INTO Rooms VALUES (8, 'Apartment',0,0);
+INSERT INTO Rooms VALUES (9,'Apartment',0,0);
+INSERT INTO Rooms VALUES (10, 'Single',0, 0);
+INSERT INTO Rooms VALUES (11,'Double Room',0,0);
+INSERT INTO Rooms VALUES (12,'Apartment',0,0);
+INSERT INTO Rooms VALUES (13, 'Single',0, 0);
+INSERT INTO Rooms VALUES (14,'Apartment',0, 0);
+INSERT INTO Rooms VALUES (15, 'Single',0, 0);
+
+Select * from Rooms
