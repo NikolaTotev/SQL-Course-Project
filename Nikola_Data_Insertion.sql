3b1b864c2d58ef3d2f86201da24f08705f5101c0
Nikola <nikola.r.totev@gmail.com>
2020-05-06 14:24:09 +0300

    Create Nikola_Data_Insertion.sql

---
+++ Nikola_Data_Insertion.sql
@@ -0,0 +1,9 @@
+use HotelDB
+go
+
+INSERT INTO RoomTypes VALUES ('Single',1, 15);
+INSERT INTO RoomTypes VALUES ('Double Room',2, 25);
+INSERT INTO RoomTypes VALUES ('Apartment',4, 50);
+INSERT INTO RoomTypes VALUES ('Pent House',10, 150);
+
+select * from RoomTypes
