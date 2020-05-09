b38585628be6873304a1fe216ac6a936bee8b122
Nikola <nikola.r.totev@gmail.com>
2020-05-09 17:20:11 +0300

    Update Nikola_View_Query.sql

--- Nikola_View_Query.sql
+++ Nikola_View_Query.sql
@@ -1,6 +1,6 @@
 Create View GuestInfo As 
        Select Guests.FirstName, Guests.LastName, Reservations.RoomNumber, Reservations.CheckInDate, Reservations.CheckOutDate  from Reservations join guests on Reservations.GuestID=Guests.ID
 
-select* from guestinfo
+Create View RoomInfo As 
+       Select Rooms.roomNumber, RoomTypes.roomType, RoomTypes.numberOfBeds, RoomTypes.pricePerNight  from Rooms join RoomTypes on Rooms.roomType=RoomTypes.roomtype
 
-	   
